72398da43bb80025fe748a38acf11c09b6f53441
Pawel Durkalec <pdurkalec1006@gmail.com>
2022-07-28 15:00:28 +0200

    Bot

---
+++ WoT_BooT.py
@@ -0,0 +1,99 @@
+import ctypes
+import time
+import random
+import pyautogui
+
+SendInput = ctypes.windll.user32.SendInput
+
+
+W = 0x11
+A = 0x1E
+S = 0x1F
+D = 0x20
+
+NP_2 = 0x50
+NP_4 = 0x4B
+NP_6 = 0x4D
+NP_8 = 0x48
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+if __name__ == '__main__':
+    PressKey(0x11)
+    time.sleep(1)
+    ReleaseKey(0x11)
+    time.sleep(1)
+
+time.sleep(5)
+
+while True:
+    pyautogui.mouseDown(957, 52)
+    pyautogui.mouseUp(957, 52)
+    czas=random.randint(2,5)
+    losowa=random.randint(1,7)
+    if losowa==1:
+        PressKey(0x1F)
+        time.sleep(czas)
+        ReleaseKey(0x1F)
+    elif losowa==2:
+        PressKey(0x1E)
+        time.sleep(czas)
+        ReleaseKey(0x1E)
+    elif losowa==3:
+        PressKey(0x20)
+        time.sleep(czas)
+        ReleaseKey(0x20)
+    elif losowa==4:
+        PressKey(0x15)
+        time.sleep(czas)
+        ReleaseKey(0x15)
+    else:
+        PressKey(0x11)
+        time.sleep(czas)
+        ReleaseKey(0x11)
+       
